ac60e525de226254e99d6c194a658a9de6834ae4
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-05-17 21:19:45 +0800

    Filter search bar functionality.

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ function App() {
   const API_URL = "https://api.themoviedb.org/3";
 
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   //Fetch movies
-  const fetchMovies = async () => {
+  const fetchMovies = async (search) => {
+    const filterMovie = search ? "search" : "discover";
     const {
       data: { results },
-    } = await axios.get(`${API_URL}/discover/movie`, {
+    } = await axios.get(`${API_URL}/${filterMovie}/movie`, {
       params: {
         api_key: API_KEY,
+        query: search,
       },
     });
     setMovies(results);
@@ -27,9 +30,15 @@ function App() {
     fetchMovies();
   }, []);
 
+  //Filter movie search
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(search);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <Header searchMovies={searchMovies} setSearch={setSearch} />
       <RenderMovies movies={movies} />
     </div>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,12 @@
 import "./Styles/Header.scss"
 
-function Header() {
+function Header({setSearch, searchMovies}) {
   return (
     <div className="header max-content">
         <div className="header-content">
             <h1>FilmSnack</h1>
-            <form>
-                <input type="text" placeholder="e.g. Spiderman"/>
+            <form onSubmit={searchMovies}>
+                <input type="text" placeholder="e.g. Spiderman" onChange={(e) => setSearch(e.target.value)}/>
                 <button type="submit">Search</button>
             </form>
         </div>
