170447376cdc867fd0adf502c5436d7417dd4d1b
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-05-23 16:34:42 +0800

    Responsive movie trailer.

--- src/Components/TrailerMovie.jsx
+++ src/Components/TrailerMovie.jsx
@@ -1,7 +1,6 @@
 import "../Assets/Styles/TrailerMovie.scss"
 
 function TrailerMovie({selectMovie}) {
-  console.log(selectMovie);
   //For trailer
   const trailer = selectMovie.videos.results.find(video => video.name === "Official Trailer")
 
@@ -11,21 +10,22 @@ function TrailerMovie({selectMovie}) {
   ))
 
   return (
-    <div className="modal">
+    <div className="youtube">
+      <div className="modal">
         <div className="modal-wrapper">
-            <div className="top-modal">X</div>
-            <div className="body-modal">
-              <div className="main-trailer">
-                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${trailer.key}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </div>
-              <div className="movie-details">
-                <h3>{selectMovie.original_title}</h3>
-                <small>"{selectMovie.tagline}"</small>
-              </div>
-              <span className="movie-runtime">{selectMovie.runtime}mins</span>
-              <div className="genre-container">{key}</div>
-            </div>
+          <div className="main-trailer">
+            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${trailer.key}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
         </div>
+        <div className="body-modal">
+          <div className="movie-details">
+            <h3>{selectMovie.original_title}</h3>
+            <small>"{selectMovie.tagline}"</small>
+          </div>
+          <div className="genre-container">{key}</div>
+          <span className="movie-runtime">{selectMovie.runtime}mins</span>
+        </div>
+      </div>
     </div>
   )
 }
