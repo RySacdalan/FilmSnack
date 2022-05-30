d2d0c2fb8f8e874820a13272ded1defe784777ce
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-05-30 14:44:07 +0800

    Add logo and favicon.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,11 +4,14 @@ function Header({setSearch, searchMovies}) {
   return (
     <div className="header max-content">
         <div className="header-content">
+          <div className="header-title">
+            <ion-icon name="videocam"></ion-icon>
             <h1>FilmSnack</h1>
-            <form onSubmit={searchMovies}>
-                <input className="input-form" type="text" placeholder="e.g. Spiderman" onChange={(e) => setSearch(e.target.value)}/>
-                <button type="submit">Search</button>
-            </form>
+          </div>
+          <form onSubmit={searchMovies}>
+            <input className="input-form" type="text" placeholder="e.g. Spiderman" onChange={(e) => setSearch(e.target.value)}/>
+            <button type="submit">Search</button>
+          </form>
         </div>
     </div>
   )
